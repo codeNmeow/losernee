98390d57e4be91bf0a03cfc202f70a51e2f74049
codeNmeow <coded.meow@gmail.com>
2024-03-06 13:38:17 +0600

    fokinnir bachcha ami

---
+++ losernirBachcha.c
@@ -0,0 +1,28 @@
+//loser er bachcha loser. spit on myface. fucking loser
+
+#include <stdio.h>
+
+int main(){
+
+    double arr[] = {43.08, 64.1, 28.53, 42.9, 3.04, 73.73};
+    printf("Hola World!\n\n");
+
+    for (int i = 0; i<sizeof(arr) / sizeof(arr[0]); i++){
+        printf("total: %.2f \n\n", arr[i]);
+    }
+    return -1;
+}
+
+
+// #include <stddef.h>
+// #include <stdio.h>
+
+// int main()
+// {
+// 	int array[5] = { 1, 2, 3, 4, 5 };
+// 	size_t size = sizeof(array);
+
+// 	printf("The size of the array is: %lu\n", size);
+
+// 	return 0;
+// }
